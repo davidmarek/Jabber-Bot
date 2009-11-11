94a71db15b13e153ad1b8cf7b89b2e520e4075ba
David Marek <davidm@atrey.karlin.mff.cuni.cz>
2009-11-11 15:15:50 +0100

    Osetreno parsovani stranky i pro novejsi verzi BeautifulSoup, bude potreba zpracovani stranky vytahnout do samostatne tridy.

--- plugins/mdt.py
+++ plugins/mdt.py
@@ -10,8 +10,19 @@ class MDT:
     def __init__(self):
         print 'mdt init'
 
+    def __filter_source(self, text):
+        start = 0
+        end = text.find("<script", start) 
+        novy = ""
+        while end != -1:
+            novy += text[start:end]
+            start = text.find("</script>", end) + len("</script>")
+            end = text.find("<script", start) 
+        novy += text[start:]
+        return novy
+
     def run(self, contact, msg):
-        web_page = urllib.urlopen('http://mydrunktexts.com/random').read()
+        web_page = self.__filter_source(urllib.urlopen('http://mydrunktexts.com/random').read())
         soup = bs.BeautifulSoup(web_page)
         txt = soup.find(attrs={'class':'fmllink'}).string
         txt = txt.replace('&quot;','"')
